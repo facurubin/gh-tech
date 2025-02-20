8ea7feea9da8631c3a0f99c8f8f8d20dd14d1382
Facundo Rubin <facundo.rubin@treinta.co>
2025-02-20 19:40:57 -0300

    Initial commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,27 @@
+import { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import {
+  DocumentHeadTags,
+  DocumentHeadTagsProps,
+  documentGetInitialProps,
+} from '@mui/material-nextjs/v15-pagesRouter';
+import { JSX } from "react";
+
+export default function Document(props: JSX.IntrinsicAttributes & DocumentHeadTagsProps) {
+  return (
+    <Html lang="en">
+      <Head>
+        <DocumentHeadTags {...props} />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+
+Document.getInitialProps = async (ctx: DocumentContext) => {
+  const finalProps = await documentGetInitialProps(ctx);
+  return finalProps;
+};
